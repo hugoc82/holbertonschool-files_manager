88e4540b22a1b60bd66bb8a5a1ac572a323ceccf
hugoc82 <hugocapelle@orange.fr>
2025-09-05 12:34:18 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// NEW: create user
+router.post('/users', UsersController.postNew);
 
 export default router;
 
