c6f3032be47135e77daefb695cc43af360b1380d
hugoc82 <hugocapelle@orange.fr>
2025-09-05 12:56:58 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = Router();
 
@@ -10,10 +11,12 @@ router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
 
-// NEW auth routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+// NEW: upload / create file or folder
+router.post('/files', FilesController.postUpload);
 
 export default router;
 
